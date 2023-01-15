544d20a199101efd213d7a58de2f245398734fc3
Tanngar <sashak95@gmail.com>
2023-01-15 22:19:17 +0100

    moved procfile

--- server/public/postings/postings.route.js
+++ server/public/postings/postings.route.js
@@ -1,20 +1,26 @@
 import express from 'express';
-import { getPostings, getPosting, createPosting, editPosting, deletePosting, } from './postings.controller';
+import {
+  getPostings,
+  getPosting,
+  createPosting,
+  editPosting,
+  deletePosting,
+} from './postings.controller';
 const router = express.Router();
 router.get('/', async (req, res) => {
-    getPostings(req, res);
+  getPostings(req, res);
 });
 router.get('/:id', async (req, res) => {
-    getPosting(req, res);
+  getPosting(req, res);
 });
 router.post('/', (req, res) => {
-    createPosting(req, res);
+  createPosting(req, res);
 });
 router.put('/:id', (req, res) => {
-    editPosting(req, res);
+  editPosting(req, res);
 });
 router.delete('/:id', (req, res) => {
-    deletePosting(req, res);
+  deletePosting(req, res);
 });
 export default router;
 //# sourceMappingURL=postings.route.js.map
